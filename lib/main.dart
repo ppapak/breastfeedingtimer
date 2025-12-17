6568834d482fe2e8e4bfae7707499c5057cd7ce4
p.papakonstantinou <p.papakonstantinou@gmail.com>
2025-12-17 15:33:28 +0000

    fixed styling

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,8 @@ class MyApp extends StatelessWidget {
     const Color primarySeedColor = Colors.deepPurple;
 
     final TextTheme appTextTheme = TextTheme(
-      displayLarge: GoogleFonts.oswald(fontSize: 57, fontWeight: FontWeight.bold),
+      displayLarge:
+          GoogleFonts.oswald(fontSize: 57, fontWeight: FontWeight.bold),
       titleLarge: GoogleFonts.roboto(fontSize: 22, fontWeight: FontWeight.w500),
       bodyMedium: GoogleFonts.openSans(fontSize: 14),
     );
@@ -47,7 +48,8 @@ class MyApp extends StatelessWidget {
       appBarTheme: AppBarTheme(
         backgroundColor: primarySeedColor,
         foregroundColor: Colors.white,
-        titleTextStyle: GoogleFonts.oswald(fontSize: 24, fontWeight: FontWeight.bold),
+        titleTextStyle:
+            GoogleFonts.oswald(fontSize: 24, fontWeight: FontWeight.bold),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
@@ -55,7 +57,8 @@ class MyApp extends StatelessWidget {
           backgroundColor: primarySeedColor,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-          textStyle: GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500),
+          textStyle:
+              GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500),
         ),
       ),
     );
@@ -70,7 +73,8 @@ class MyApp extends StatelessWidget {
       appBarTheme: AppBarTheme(
         backgroundColor: Colors.grey[900],
         foregroundColor: Colors.white,
-        titleTextStyle: GoogleFonts.oswald(fontSize: 24, fontWeight: FontWeight.bold),
+        titleTextStyle:
+            GoogleFonts.oswald(fontSize: 24, fontWeight: FontWeight.bold),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
@@ -78,7 +82,8 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepPurple.shade200,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-          textStyle: GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500),
+          textStyle:
+              GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500),
         ),
       ),
     );
@@ -149,7 +154,8 @@ class _MyHomePageState extends State<MyHomePage> {
               child: CircleAvatar(
                 backgroundImage: babyProvider.babyPhotoPath != null
                     ? FileImage(File(babyProvider.babyPhotoPath!))
-                    : const AssetImage('assets/images/icon2.png') as ImageProvider,
+                    : const AssetImage('assets/images/icon2.png')
+                        as ImageProvider,
               ),
             ),
             const SizedBox(width: 10),
@@ -172,7 +178,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         actions: [
           IconButton(
-            icon: Icon(themeProvider.themeMode == ThemeMode.dark ? Icons.light_mode : Icons.dark_mode),
+            icon: Icon(themeProvider.themeMode == ThemeMode.dark
+                ? Icons.light_mode
+                : Icons.dark_mode),
             onPressed: () => themeProvider.toggleTheme(),
             tooltip: 'Toggle Theme',
           ),
@@ -182,7 +190,8 @@ class _MyHomePageState extends State<MyHomePage> {
               final summary = "Baby's Feeding Summary:\n"
                   "Feeds in last 24h: ${history.feedsInLast24Hours}\n"
                   "Total duration today: ${history.totalTodayDuration.inMinutes} minutes";
-              Share.share(summary, subject: 'Baby Feeding Summary');
+              SharePlus.instance.share(
+                  (summary, subject: 'Baby Feeding Summary') as ShareParams);
             },
             tooltip: 'Share',
           ),
@@ -232,9 +241,10 @@ class TimerSection extends StatelessWidget {
     );
   }
 
-  Widget _buildBreastButton(BuildContext context, BreastSide side, TimerModel timer, HistoryModel history) {
+  Widget _buildBreastButton(BuildContext context, BreastSide side,
+      TimerModel timer, HistoryModel history) {
     final isSelected = timer.isRunning && timer.currentSide == side;
-    const buttonSize = 150.0;
+    const buttonSize = 160.0;
 
     final totalLeft = history.totalDurationForSide(BreastSide.left);
     final totalRight = history.totalDurationForSide(BreastSide.right);
@@ -250,11 +260,11 @@ class TimerSection extends StatelessWidget {
     }
 
     String formatDuration(Duration d) {
-        if (d.inMinutes > 0) {
-            return "${d.inMinutes}m ${d.inSeconds.remainder(60)}s";
-        } else {
-            return "${d.inSeconds}s";
-        }
+      if (d.inMinutes > 0) {
+        return "${d.inMinutes}m ${d.inSeconds.remainder(60)}s";
+      } else {
+        return "${d.inSeconds}s";
+      }
     }
 
     return SizedBox(
@@ -266,7 +276,11 @@ class TimerSection extends StatelessWidget {
           CircularProgressIndicator(
             value: percentage,
             strokeWidth: 15,
-            backgroundColor: Theme.of(context).colorScheme.surfaceContainerHighest.withAlpha(128),
+            strokeAlign: -3,
+            backgroundColor: Theme.of(context)
+                .colorScheme
+                .surfaceContainerHighest
+                .withAlpha(128),
             valueColor: AlwaysStoppedAnimation<Color>(
               side == BreastSide.left
                   ? Theme.of(context).colorScheme.primary
@@ -290,14 +304,22 @@ class TimerSection extends StatelessWidget {
               },
               style: ElevatedButton.styleFrom(
                 shape: const CircleBorder(),
-                padding: const EdgeInsets.all(30),
-                backgroundColor: isSelected ? Theme.of(context).colorScheme.tertiary : Theme.of(context).colorScheme.surface,
-                foregroundColor: isSelected ? Theme.of(context).colorScheme.onTertiary : Theme.of(context).colorScheme.onSurface,
+                padding: const EdgeInsets.all(40),
+                backgroundColor: isSelected
+                    ? Theme.of(context).colorScheme.tertiary
+                    : Theme.of(context).colorScheme.surface,
+                foregroundColor: isSelected
+                    ? Theme.of(context).colorScheme.onTertiary
+                    : Theme.of(context).colorScheme.onSurface,
                 elevation: 8,
               ),
               child: isSelected
-                  ? Text(formatDuration(timer.duration), style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold))
-                  : Text(side == BreastSide.left ? "L" : "R", style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold)),
+                  ? Text(formatDuration(timer.duration),
+                      style: const TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.bold))
+                  : Text(side == BreastSide.left ? "L" : "R",
+                      style: const TextStyle(
+                          fontSize: 60, fontWeight: FontWeight.bold)),
             ),
           ),
         ],
@@ -308,7 +330,8 @@ class TimerSection extends StatelessWidget {
   Widget _buildAddButton(BuildContext context) {
     return FloatingActionButton(
       onPressed: () async {
-        showDialog(context: context, builder: (context) => const ManualEntryDialog());
+        showDialog(
+            context: context, builder: (context) => const ManualEntryDialog());
       },
       elevation: 8,
       child: const Icon(Icons.add, size: 40),
